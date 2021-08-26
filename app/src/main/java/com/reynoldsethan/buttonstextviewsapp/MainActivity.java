f3206819ac0ef45ebbca982e54ee1da78b9533f0
Reynolds <1507938@fcps.edu>
2021-08-26 14:04:24 -0400

    Initial commit

---
+++ app/src/main/java/com/reynoldsethan/buttonstextviewsapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.reynoldsethan.buttonstextviewsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button increase_power;
+    Button decrease_power;
+    TextView power;
+    int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        increase_power = findViewById(R.id.new_button);
+        decrease_power = findViewById(R.id.lower_button);
+        power = findViewById(R.id.new_view);
+        decrease_power.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count-=1000;
+                System.out.println("POWER: " + count);
+                Log.i("decreasing_power","" + count);
+                if(count>=9000)
+                    power.setText("IT'S OVER 9000: " + count);
+                else
+                    power.setText("POWER: " + count);
+            }
+        });
+        increase_power.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count+=1000;
+                System.out.println("POWER: " + count);
+                Log.i("increasing_power","" + count);
+                if(count>=9000)
+                    power.setText("IT'S OVER 9000: " + count);
+                else
+                    power.setText("POWER: " + count);
+            }
+        });
+    }
+}
